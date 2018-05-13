547824edf37f1ff990c7f496adac1fdb38d59517
Enes Aldemir <aldemirenes@live.com>
2018-05-13 12:23:20 +0300

    Unnecessary files are removed

--- vms/client/client_init.sh
+++
@@ -1,5 +0,0 @@
-./if_destroy.sh
-./if_init.sh
-cd simpletun
-make all
-sudo ./client -i tun0 -s 10.10.1.2 -p 5555 -u -d
--- vms/server/server_init.sh
+++
@@ -1,5 +0,0 @@
-./if_destroy.sh
-./if_init.sh
-cd simpletun
-make all
-./server -i tun0 -p 5555 -u -d
